74b67807b2263b42b081833529423490b74e3943
Z User <z@container>
2026-05-20 19:58:59 +0000

    Add Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,24 @@ export default function RootLayout({
           src="https://spaceremit.com/api/v2/js_script/spaceremit.js"
           strategy="afterInteractive"
         />
+        {/* Tawk.to Live Chat Widget */}
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a0e11de2370201c349f28e6/1jp3fbk93';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
